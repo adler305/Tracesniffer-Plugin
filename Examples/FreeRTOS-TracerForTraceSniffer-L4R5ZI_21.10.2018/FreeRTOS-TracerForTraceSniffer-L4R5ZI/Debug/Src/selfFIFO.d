e37da032ba257110374483953e27ec4bcb12cbca
Penlane <penlane@protonmail.com>
2019-12-11 22:08:08 +0100

    Remove binaries from repo (they have no business here...)

--- Examples/FreeRTOS-TracerForTraceSniffer-L4R5ZI_21.10.2018/FreeRTOS-TracerForTraceSniffer-L4R5ZI/Debug/Src/selfFIFO.d
+++
@@ -1,4 +0,0 @@
-Src/selfFIFO.o: ../Src/selfFIFO.c \
- C:/Users/JonasPC/Documents/Workspaces/OpenSTM_l4r5zi/FreeRTOS-TracerForTraceSniffer-L4R5ZI/Inc/selfFIFO.h
-
-C:/Users/JonasPC/Documents/Workspaces/OpenSTM_l4r5zi/FreeRTOS-TracerForTraceSniffer-L4R5ZI/Inc/selfFIFO.h:
--- Examples/FreeRTOS-TracerForTracesniffer-F767ZI_21.10.2018/FreeRTOS-TracerForTracesniffer-F767ZI/Debug/Src/selfFIFO.d
+++
@@ -1,4 +0,0 @@
-Src/selfFIFO.o: ../Src/selfFIFO.c \
- C:/Users/JonasPC/Documents/Workspaces/OpenSTM_l4r5zi/FreeRTOS-TracerForTracesniffer-F767ZI/Inc/selfFIFO.h
-
-C:/Users/JonasPC/Documents/Workspaces/OpenSTM_l4r5zi/FreeRTOS-TracerForTracesniffer-F767ZI/Inc/selfFIFO.h:
